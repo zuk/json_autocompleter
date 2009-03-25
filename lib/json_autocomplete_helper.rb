93c8a0f7402c60855802f496c3a39dd573c20609
zuk <zuk@roughest.net>
2009-03-25 16:30:12 -0400

    added autocomplete helper... kind of a mess right now and needs documentation but works

---
+++ lib/json_autocomplete_helper.rb
@@ -0,0 +1,86 @@
+module JsonAutocompleteHelper
+  
+  def json_autocomplete(record, association, choices_url, options = {})
+    current_id      = record.send("#{association}_id")
+    current_name    = record.send(association).to_s
+    
+    dom_id_base     = options[:dom_id_base]   || dom_id(record)
+    dom_name_base   = options[:dom_name_base] || record.class.name.underscore
+    
+    base_field_id   = "#{dom_id_base}_#{association}"
+    
+    id_field_name   = "#{dom_name_base}[#{association}_id]"
+    type_field_name = "#{dom_name_base}[#{association}_type]"
+    
+    js_var_name     = options[:js_var_name] || base_field_id;
+    
+    width           = options[:width] || "30ex"
+    
+    as_user_id      = options[:as_user]
+    as_user_id      = as_user_id.id if as_user_id.kind_of?(User)
+    
+    
+    if as_user_id
+      choices_url << (choices_url.include?('?') ? "&" : "?") + "perform_as_user_id=#{as_user_id}"
+    end
+    
+    if options[:with]
+      with = "'#{choices_url.include?('?') ? "&" : "?"}' + #{options[:with]}"
+    else
+      with = "''"
+    end
+    
+    if options[:with_form_elements]
+      wfe = ""
+      if options[:with_form_elements].kind_of?(Hash)
+        wfe << "{"
+        options[:with_form_elements].each do |k,e|
+          wfe << "'#{k}': $('#{e}'),"
+        end
+        wfe << "}"
+      elsif options[:with_form_elements].kind_of?(Array)
+        wfe << "{"
+        options[:with_form_elements].each do |e|
+          wfe << "$('#{k}').name: $('#{e}'),"
+        end
+        wfe << "}"
+      end
+    else
+      wfe = 'null'
+    end
+    with_form_elements = wfe
+    
+    
+    # modify the URL to make sure we're requesting a response in JSON format
+    choices_url.sub!(/\?|$/, ".json#{'?' if choices_url.include?('?')}") unless choices_url.include? '.json'
+    
+    style = options[:style] || ""
+    
+    if width
+      style << "; width: #{width}"
+    end
+    
+    type_field = %{<input type="hidden" name="#{type_field_name}" id="#{base_field_id}_type" class="type" />} if options[:include_type_field]
+    
+    container = %{
+      <div class="auto_complete" id="#{base_field_id}_autocomplete_container" style="#{style}">
+        <input type="text"   name="#{base_field_id}_autocomplete" style="width: #{width}" id="#{base_field_id}_autocomplete" class="text" value="#{current_name}" />
+        <input type="hidden" name="#{id_field_name}" id="#{base_field_id}_id" class="id" value="#{current_id}" />
+        #{type_field}
+        <div class="choices_container" id="#{base_field_id}_options"></div>
+      </div>
+    }
+    
+    javascript = javascript_tag %{
+      var #{js_var_name} = new JSON.Autocompleter(
+        '#{base_field_id}_autocomplete_container', 
+        '#{base_field_id}_options', 
+        '#{choices_url}',
+        {'parameters': #{with}, 'withFormElements': #{with_form_elements}}
+      )
+    }
+    
+    return container + javascript
+  end
+  
+end
