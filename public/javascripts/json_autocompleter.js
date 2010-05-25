1dde4e83d71a0dba51e82d41613f4e9c91e85465
Matt Zukowski <matt@zukowski.ca>
2010-05-25 10:53:44 -0700

    explicitly trigger onchange event on id field when the id value changes, since this might not be triggered otherwise (but should be!)

--- public/javascripts/json_autocompleter.js
+++ public/javascripts/json_autocompleter.js
@@ -153,7 +153,8 @@ JSON.Autocompleter = Class.create(Autocompleter.Base, {
     this.id_field.value = id_value
 
     if (previous_id_value != id_value && this.options.onchange) {
-      this.options.onchange(id_value);
+      this.options.onchange(this);
+      this.id_field.up('form').onchange() // notify the parent form that someting has changed
     }
 
     this.oldElementValue = this.element.value;
